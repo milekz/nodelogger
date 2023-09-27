12ae9ffbd884ff2c65a1e2b669e5399ef75903a8
Milosz <milek@milek.eu>
2023-09-27 08:43:53 +0200

    mem table

--- logger.js
+++ logger.js
@@ -1,75 +1,100 @@
-const express = require('express');
-const morgan = require('morgan');
-const fs = require('fs');
-const path = require('path');
-const http = require('http');
-const socketIo = require('socket.io');
-const bodyParser = require('body-parser');
-const readline = require('readline');
+const express = require("express");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+const http = require("http");
+const socketIo = require("socket.io");
+const bodyParser = require("body-parser");
+const readline = require("readline");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-const logfilename='log/access.log'
+const logfilename = "log/access.log";
+
+// In-memory variable to store the last 50 log entries
+const logEntries = [];
 
 // Create a write stream in append mode
-const accessLogStream = fs.createWriteStream(path.join(__dirname, logfilename), { flags: 'a' });
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, logfilename),
+  { flags: "a" }
+);
 
 // Define a new token to log request body in morgan
-morgan.token('body', (req) => (req.body && Object.keys(req.body).length > 0) ? JSON.stringify(req.body) : '');
+morgan.token("body", (req) =>
+  req.body && Object.keys(req.body).length > 0 ? JSON.stringify(req.body) : ""
+);
 
-// Setup the logger to log to both the file and the console in a single line including the body.
-//app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', { stream: accessLogStream }));
-app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] - :response-time ms body: :body', { stream: accessLogStream }));
-
-
+// Setup the logger to log to both the file and the console in a single line including IP, date, and body.
+app.use(
+  morgan(
+    ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] - :response-time ms :body',
+    {
+      stream: {
+        write: (message) => {
+          // Write to the file
+          accessLogStream.write(message);
+          // Update the in-memory variable
+          logEntries.push(message.trim());
+          if (logEntries.length > 50) logEntries.shift();
+          // Emit to all connected clients
+          io.emit("update", logEntries.join("\n"));
+        },
+      },
+    }
+  )
+);
 
 // Parse JSON bodies (as sent by API clients)
 app.use(bodyParser.json());
 
 // Serve static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Setup routes
-app.get('/', (req, res) => {
-  res.send('GET request received!');
+app.get("/", (req, res) => {
+  res.send("GET request received!");
 });
 
-app.post('/', (req, res) => {
-  res.send('POST request received!');
+app.post("/", (req, res) => {
+  res.send("POST request received!");
 });
 
-app.delete('/', (req, res) => {
-  res.send('DELETE request received!');
+app.delete("/", (req, res) => {
+  res.send("DELETE request received!");
 });
 
-app.put('/', (req, res) => {
-  res.send('PUT request received!');
+app.put("/", (req, res) => {
+  res.send("PUT request received!");
 });
 
-io.on('connection', (socket) => {
-  console.log('New client connected');
-  sendLastLines(socket);
-  fs.watchFile(path.join(__dirname, logfilename), () => {
-    sendLastLines(socket);
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  // Emit the in-memory log entries to the newly connected client
+  socket.emit("update", logEntries.join("\n"));
+});
+
+// Load the last 50 log entries from the file into memory at the start
+const loadLastLines = () => {
+  const fileStream = fs.createReadStream(path.join(__dirname, logfilename), {
+    encoding: "utf-8",
   });
-});
-
-const sendLastLines = (socket) => {
-  const fileStream = fs.createReadStream(path.join(__dirname, logfilename), { encoding: 'utf-8' });
   const rl = readline.createInterface({ input: fileStream });
   const lines = [];
-  rl.on('line', (line) => {
+  rl.on("line", (line) => {
     lines.push(line);
-    if (lines.length > 50) lines.shift(); // Keep only the last 50 lines
+    if (lines.length > 50) lines.shift();
   });
-  rl.on('close', () => {
-    socket.emit('update', lines.join('\n'));
+  rl.on("close", () => {
+    logEntries.push(...lines);
   });
 };
 
+loadLastLines();
+
 const PORT = 11000;
-server.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running at http://0.0.0.0:${PORT}`);
 });
